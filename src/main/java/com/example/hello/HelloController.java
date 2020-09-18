c3e8c8fac48ee5ed402ca826d2bded0255e1520e
patrick_hayes <patrick@vizsense.com>
2020-09-18 14:59:42 -0700

    here is to the notifications

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -3,6 +3,7 @@ package com.example.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// here is to the notifications
 @RestController
 public class HelloController {
 
