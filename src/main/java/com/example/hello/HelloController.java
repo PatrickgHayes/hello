3ee685be9ec4a5a1556d8af1f5f724aeefa219b6
patrick_hayes <patrick@vizsense.com>
2020-09-18 16:13:20 -0700

    here's to plugins

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -3,7 +3,7 @@ package com.example.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// here is to the pull requests
+// here is to the plugins
 @RestController
 public class HelloController {
 
